2e1fd54cddc7ff32e2ccd9d3f44573f302ffda4b
IXti <ixti@example.com>
2020-10-01 23:44:06 +0300

    Сделал Задание 1 для раздела 03_Условные операторы

--- 031_ConditionsTask/ConditionsTask/Program.cs
+++ 031_ConditionsTask/ConditionsTask/Program.cs
@@ -12,6 +12,8 @@ namespace ConditionsTask
         {
 
             Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Задание 1:");
+            Console.WriteLine("----------------------------------------------");
             Console.Write("Введите первое число: ");
             float num1 = Convert.ToSingle(Console.ReadLine());
 
@@ -20,6 +22,11 @@ namespace ConditionsTask
             
             Console.WriteLine("----------------------------------------------");
             Console.WriteLine("Ваши числа: " + num1 + " и " + num2);
+
+            //Пример использования тернарного оператора
+            float result = (num2 != 0) ? (num1 / num2) : 1;
+ 
+            Console.WriteLine("Результат деления чисел: {0}", result);
             Console.WriteLine("----------------------------------------------");
 
             Console.ReadKey();
