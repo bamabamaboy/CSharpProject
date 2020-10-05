d34fba7a6d94bf21fb038104da56a7c1ca5bb377
IXti <ixti@example.com>
2020-10-05 23:15:33 +0300

    Закончил Задание 2 раздела 4

--- 042_ArraysTask/ArraysTask/Program.cs
+++ 042_ArraysTask/ArraysTask/Program.cs
@@ -10,7 +10,41 @@ namespace ArraysTask
     {
         static void Main(string[] args)
         {
+            //Динамический массив
+            List<float> floatArray = addArray(4.5f, 545.849f, 38.56f, 78.86f, 42.65986f, 84.613f, 1.345f, 7.645f);
+
+            printArray(floatArray);
+            Console.WriteLine();
+
+            floatArray.Sort();
+
+            printArray(floatArray);
+
             Console.ReadKey();
+        }
+
+        public static List<float> addArray(params float[] fArray) {
+            List<float> list = new List<float>();
+
+            foreach (float f in fArray) {
+                list.Add(f);
+            }
+
+            return list;
+        }
+
+        public static void printArray(List<float> list) {
+            Console.Write("Массив: [ ");
+            
+            for (short i = 0; i < list.Count; i++)
+            {
+                Console.Write("{0}", list.ElementAt(i));
+
+                if (i != (list.Count - 1)) 
+                    Console.Write(" | ");
+            }
+
+            Console.WriteLine("]");
         }
     }
 }
