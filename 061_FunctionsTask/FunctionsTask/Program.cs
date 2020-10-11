e8e26a8c312c305c9df04c65b76247cca3da7eb1
IXti <ixti@example.com>
2020-10-11 23:55:38 +0300

    Задание 1 к разделу 06_Функции

---
+++ 061_FunctionsTask/FunctionsTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите значение скорости: ");
+            int speed = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите значение времени: ");
+            int time = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваша скорость: {0} км/ч, Ваше время в пути: {1} ч", speed, time);
+
+            int way = wayCount(speed, time);
+            Console.WriteLine("Рассчитанное значение: {0}", way);
+
+            Console.ReadKey();
+        }
+
+        private static int wayCount(int speed, int time) { 
+            int way = speed * time;
+
+            Console.WriteLine("Вы проедите: {0} км", way);
+
+            return way;
+        }
+
+    }
+}
