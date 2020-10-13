f8c43707e1fe03a061fa8b2575cae981b0b95a94
IXti <ixti@example.com>
2020-10-13 23:56:49 +0300

    Задание к Разделу 08 - Классы и объекты

---
+++ 081_ClassTask/ClassTask/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    class Car
+    {
+        private string model;
+        private int weight;
+        private string color;
+        public float maxSpeed;
+
+        public void setAllInfo(string model, int weight, string color, float maxSpeed) {
+            this.model = model;
+            this.weight = weight;
+            this.color = color;
+            this.maxSpeed = maxSpeed;
+        }
+
+        public void getCarInfo() {
+            Console.WriteLine("Ваша машина - {0}, имеет характеристики:", model);
+            Console.WriteLine("Масса: {0} кг, Цвет: {1}, Максимальная скорость: {2} км/ч", weight, color, maxSpeed);
+        }
+
+    }
+}
---
+++ 081_ClassTask/ClassTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример создания объектов класса Car
+            List<Car> garage = new List<Car>();
+            
+            //Авто 1
+            Car car = new Car();
+            car.setAllInfo("Ауди ТТ", 1600, "Чёрная", 240);
+            garage.Add(car);
+
+            //Авто 2
+            car = new Car();
+            car.setAllInfo("Ферари", 1900, "Красная", 280);
+            garage.Add(car);
+
+            //Авто 3
+            car = new Car();
+            car.setAllInfo("Ламборгини", 2100, "Жёлтая", 310);
+            garage.Add(car);
+
+            //Вывод информации о всех Авто в гараже
+            byte i = 0;
+            foreach (Car c in garage){
+                i++;
+                Console.WriteLine("----------- Машина в гараже №{0} -----------", i);
+                c.getCarInfo();
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
