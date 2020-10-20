95e312a42bd78af482c8ae18d4e2ce695e2e4514
IXti <ixti@example.com>
2020-10-20 23:15:57 +0300

    Задание к разделу 09 - Конструкторы, Модификаторы, Аксессоры

--- 091_ClassConstructorsTask/ClassConstructorsTask/House.cs
+++ 091_ClassConstructorsTask/ClassConstructorsTask/House.cs
@@ -8,16 +8,64 @@ namespace ClassConstructorsTask
 {
     class House
     {
-        private string type;
-        private short floors;
-        private short year;
-        private Address address;
+        //Переменные класса House --------------------------------------------
+        //Конструкция { get; set; } - это автоматические Свойства (аксессоры), их лучше всегда добавлять к полям класса
+        //чтобы в дальнейшем, если будет надо, то можно быть легко прикруть доп. проверки
+        public string type { get; set; }
+        public short floors { get; set; }
+        private short year;                                     //У поля year реализованы Свойства ниже
+        private Address address;                                //Переменная для хранения адреса объекта House
+        public static int houseCount { get; private set; }      //Переменная для подсчёта общего кол-ва объектов House
 
+        //Акссессоры (Геттеры и Cеттеры) класса House ------------------------
+        //в C# они называются Свойствами
+        public short Year {
+            get {
+                return year; 
+            }
+
+            set {
+                if (value < 0) {
+                    year = 2020;
+                }
+                else {
+                    year = value;
+                }
+            }
+        }
+
+        public Address Address {
+            get {
+                return address;
+            }
+
+            set {
+                address = value;
+            }
+        
+        }
+
+        //Конструкторы класса House ------------------------------------------
+        //Перегрузка конструктора с помощью разных аргументов
         public House() {
             type = "Жилой дом";
             floors = 1;
             year = 2020;
             address = new Address();
+
+            houseCount++;
+
+            Console.WriteLine("Был вызван пустой конструктор!");
+        }
+
+        public House(string type, short year) {
+            this.type = type;
+            this.year = year;
+            address = new Address();
+
+            houseCount++;
+
+            Console.WriteLine("Был вызван легковесный конструктор!");
         }
 
         public House(string type, short floors, short year) {
@@ -25,12 +73,26 @@ namespace ClassConstructorsTask
             this.floors = floors;
             this.year = year;
             address = new Address();
+
+            houseCount++;
+
+            Console.WriteLine("Был вызван основной конструктор!");
         }
 
-        public void setAddress(string city, string street, string number) {
-            address.setAddress(city, street, number);
+        public House(string type, short floors, short year, Address address) {
+            this.type = type;
+            this.floors = floors;
+            this.year = year;
+            this.address = address;
+
+            houseCount++;
+
+            Console.WriteLine("Был вызван расширенный конструктор!");
         }
 
+
+        //Основные методы класса House ---------------------------------------
+        //Метод считает возраст постройки
         public int countYears() { 
             int years = Convert.ToInt32(DateTime.Now.ToShortDateString().Substring(6, 4));
             years = years - year;
@@ -38,6 +100,7 @@ namespace ClassConstructorsTask
             return years;
         }
 
+        //Метод для вывода основной информации о постройке
         public void printAllInfo() {
             Console.WriteLine("Здание: {0},\nКол-во этажей: {1},\nВозраст здания: {2},\nАдрес: {3}", 
                 type, floors, countYears(), address.getAddress());
--- 091_ClassConstructorsTask/ClassConstructorsTask/Program.cs
+++ 091_ClassConstructorsTask/ClassConstructorsTask/Program.cs
@@ -10,11 +10,30 @@ namespace ClassConstructorsTask
     {
         static void Main(string[] args)
         {
-            House myHouse = new House("Жилой Дом", 14, 1972);
+            House house1 = new House();
+            house1.Year = -16;                                      //Вызов свойства set (с проверкой на значение года)
+            house1.Address = new Address("Сочи", "ул. Олимпийская деревня", "24 кр.8");
 
-            myHouse.setAddress("Москва", "ул. Халтуринская", "12 кр.1");
+            House house2 = new House("Новенький жилой Домик", 2020);
 
-            myHouse.printAllInfo();
+            House house3 = new House("Местный Кремль", 25, 1602);
+            house3.Address = new Address("Москва", "ул. Самая Красная Площадь", "1 кр.1");
+
+            Address addressHouse4 = new Address();
+            addressHouse4.setAddress("Ольгинка", "ул. Советская", "99");
+            House house4 = new House("Дом Аллигарха", 75, 2018, addressHouse4);
+
+            Console.WriteLine("Кол-во построек всего: {0}", House.houseCount);
+
+            Console.WriteLine("---------------------------------");
+            house1.printAllInfo();
+            Console.WriteLine("---------------------------------");
+            house2.printAllInfo();
+            Console.WriteLine("---------------------------------");
+            house3.printAllInfo();
+            Console.WriteLine("---------------------------------");
+            house4.printAllInfo();
+            Console.WriteLine("---------------------------------");
 
             Console.ReadKey();
         }
