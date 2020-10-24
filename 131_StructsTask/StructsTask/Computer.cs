acf7414643f64c31611ee34da8ab9f75e10380f3
IXti <ixti@example.com>
2020-10-24 22:53:11 +0300

    Задание к разделу 13 - Структуры

---
+++ 131_StructsTask/StructsTask/Computer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsTask {
+    //Структуры - это конструкции хранящие в себе переменные и методы для работы с ними
+    //Это "облегчённые" классы с урезанным и специфическим функционалом направленным для работы с переменными
+    struct Computer {
+
+        public string processor;
+        public float weight;
+        public byte workingTime;
+        public sbyte yadra;
+        public string os;
+
+    }
+}
---
+++ 131_StructsTask/StructsTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsTask {
+    class Program {
+        static void Main(string[] args) {
+
+            //Структуры не обязательно инициализировать с помощью new...
+            Computer myComp;
+
+            //но обязательно инициализировать все переменные перед использованием
+            myComp.processor = "Core i7";
+            myComp.weight = 2.3f;
+            myComp.workingTime = 7;
+            myComp.yadra = 4;
+            myComp.os = "Windows 10";
+
+            //Как то используем параметры из инициализированной структуры
+            Console.WriteLine(myComp.processor);
+
+            Console.ReadKey();
+        }
+    }
+}
