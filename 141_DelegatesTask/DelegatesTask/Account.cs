4f364afc681600afb37f0b464e5c8c08b10f0d2b
IXti <ixti@example.com>
2020-10-27 22:15:34 +0200

    Задание 1 к разделу 14 - Делегаты

---
+++ 141_DelegatesTask/DelegatesTask/Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesTask {
+    class Account {
+
+        int sum;
+
+        //Определяем (объявленяем) делегат
+        //(Создаём делегат, определяя возвращаемый тип и принимаемые аргументы)
+        public delegate void AccountStateHandler(string message);
+
+        //Создаём переменную созданного делегата
+        AccountStateHandler delegat;
+
+        public Account(int sum) {
+            this.sum = sum;
+        }
+
+        public int currentSum { 
+            get {
+                return sum;
+            }
+        }
+
+        public void put(int sum) {
+            this.sum += sum;
+        }
+
+        public void withdrawWithoutNotice(int sum) { 
+            if (sum <= this.sum) {
+                this.sum -= sum;
+            }
+        }
+
+        //При снятие денег, делигируем функционал уведомления о снятие на усмотрение разрабочиков программы извне
+        //Если в список вызовов делегата будет добавлен метод, то при вызове метода withhdraw() будет вызываться выполнение делегата
+        //Так как разработчики класса не знают, куда требуется выводить уведомление, то этот функционал остаётся на усмотрение разработчиков извне
+        //(Выводить в консоль, отраправка сообщения по email или SMS-кой на телефон, или текстом в окно приложения и т.п.)
+        public void withdraw(int sum) {
+            if (sum <= this.sum) {
+                this.sum -= sum;
+
+                if (this.delegat != null) {
+                    this.delegat("Сумма " + sum + " снята со счета");
+                }
+            } else {
+                if (this.delegat != null) {
+                    this.delegat("Недостаточно денег на счете!");
+                }
+            }
+        }
+
+        //Метод добавляет доп.метод в список вызовов делегата
+        public void registerHandler(AccountStateHandler delegat) {
+            this.delegat += delegat;
+        }
+
+        //Метод убирает доп.метод из списка вызовов делегата
+        //(и если есть несколько одинаковых методов, то убирает с конца)
+        public void unregisterHandler(AccountStateHandler delegat) {
+            this.delegat -= delegat;
+        }
+
+    }
+}
---
+++ 141_DelegatesTask/DelegatesTask/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesTask {
+    class Program {
+
+        //Определяем (объявленяем) новый делегат
+        delegate void Message();
+
+        delegate void MessageWithText(string text);
+
+        delegate int Operatin(int x, int y);
+        
+        static void Main(string[] args) {
+
+            //Примеры функционала делегата - обозначены в комментариях знаком >>>
+
+            Console.WriteLine("----------------------------------------------------------");
+            //>>> Функционал делегата - указатель на другой метод
+            
+            //Создаём переменную созданного делегата
+            Message message;
+
+            if (DateTime.Now.Hour < 10) {
+                message = textHello;
+            } else {
+                message = textByeBye;
+            }
+
+            //Выполнение метода из списка вызовов делегата
+            message();
+
+
+            Console.WriteLine("----------------------------------------------------------");
+            //>>> Функционал делегата - делигирование классом выполнения некоторого кода извне
+
+            //(Определяем дополнительную логику извне для выполнение какого-то дейтсвия внутри другого класса)
+            Account myAccount = new Account(400);
+            Account.AccountStateHandler colorDelegat = new Account.AccountStateHandler(colorMessage);
+
+            //Для первого метода в списке делегат передаём объект, в котором инициализируем переменую делегата 
+            myAccount.registerHandler(new Account.AccountStateHandler(showMessage));
+
+            //>>> Функционал делегата - добавление, удаление и поочередное выполнение нескольких методов в списке делегатов
+
+            //В методе registerHandler() добавляем метод список вызовов делегата
+            myAccount.registerHandler(colorDelegat);
+            myAccount.withdraw(200);
+            myAccount.withdraw(100);
+
+            //В методе unregisterHandler() удаляем метод из списка вызовов делегата
+            myAccount.unregisterHandler(colorDelegat);
+            myAccount.withdraw(250);
+            
+            Console.WriteLine("----------------------------------------------------------");
+            //>>> Функционал делегата - выполнение анонимных методов, которые позволяют создавать готовые экземрляры готов делегатов
+            //(Объявление анонимного метода начинается с delegate())
+            MessageWithText messageWithText = delegate(string text) {
+                Console.WriteLine(text);
+            };
+
+            messageWithText("Зупуск анонимный метод!");
+
+            Console.WriteLine("----------------------------------------------------------");
+            //>>> Функционал делегата - использование с лямбда-выражениями, которые являются упрощённой запись анонимных методов
+
+            Operatin operation = (x, y) => x * y;   //Лямбда-выражение для опеределение операции умножение двух чисел
+            Console.WriteLine("Результат умножение двух чисел - {0}", operation(10, 2));
+
+            //Интересно - если мы доабвим в список выполнения доп.метод, то результат операций выведетя только для последнего метода
+            operation += (x, y) => x + y;           //Лямбда-выражение для опеределения операции сложения двух чисел
+            Console.WriteLine("В списке выполенения делегата несколько методов, но результат только последнего, это сложение - {0}", operation(153, 47));
+
+            Console.WriteLine("----------------------------------------------------------");
+
+            Console.ReadKey();
+        }
+
+        //Метод - вывод текст в консоль "Привет"
+        private static void textHello() {
+            Console.WriteLine("Привет Привет!");
+        }
+
+        //Метод - вывод текст в консоль "Пока"
+        private static void textByeBye() {
+            Console.WriteLine("Пока Пока!");
+        }
+
+        //Метод - нужного сообщения в консоль
+        private static void showMessage(string message) {
+            Console.WriteLine(message);
+        }
+
+        //Метод - смена цвета текста в консоли
+        private static void colorMessage(string message) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+
+            Console.ResetColor();
+        }
+
+    }
+}
