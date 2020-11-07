10e3d433602a4ab2271c87358dcfe08c8babf79b
IXti <ixti@example.com>
2020-11-07 23:51:06 +0300

    Задание 1 к разделу 24 - Меню Вид для Текстового редактора

--- 240_TextEditor/TextEditor/MainWindow.xaml.cs
+++ 240_TextEditor/TextEditor/MainWindow.xaml.cs
@@ -24,6 +24,12 @@ namespace TextEditor {
 
         public MainWindow() {
             InitializeComponent();
+
+
+            textArea.Background = Brushes.White;
+            backWhite.IsChecked = true;
+            textArea.Foreground = Brushes.Black;
+            textBlack.IsChecked = true;
         }
 
         //Меню - Файл
@@ -55,6 +61,71 @@ namespace TextEditor {
         private void fontVerdane_Click(object sender, RoutedEventArgs e) {
             textArea.FontFamily = new FontFamily("Verdana");
             fontTimesNewRoman.IsChecked = false;
+        }
+
+        //Меню - Вид
+        private void backWhite_Click(object sender, RoutedEventArgs e) {
+            textArea.Background = Brushes.White;
+            backBlack.IsChecked = false;
+            backBlue.IsChecked = false;
+
+            if (textArea.Foreground == Brushes.White) {
+                textArea.Foreground = Brushes.Black;
+                textBlack.IsChecked = true;
+                textWhite.IsChecked = false;
+                textRed.IsChecked = false;
+            }
+        }
+
+        private void backBlack_Click(object sender, RoutedEventArgs e) {
+            textArea.Background = Brushes.Black;
+            backWhite.IsChecked = false;
+            backBlue.IsChecked = false;
+
+            if (textArea.Foreground == Brushes.Black) {
+                textArea.Foreground = Brushes.White;
+                textWhite.IsChecked = true;
+                textBlack.IsChecked = false;
+                textRed.IsChecked = false;
+            }
+        }
+
+        private void backBlue_Click(object sender, RoutedEventArgs e) {
+            textArea.Background = Brushes.Blue;
+            backWhite.IsChecked = false;
+            backBlack.IsChecked = false;
+        }
+
+        private void textBlack_Click(object sender, RoutedEventArgs e) {
+            textArea.Foreground = Brushes.Black;
+            textWhite.IsChecked = false;
+            textRed.IsChecked = false;
+
+            if (textArea.Background == Brushes.Black) {
+                textArea.Background = Brushes.White;
+                backWhite.IsChecked = true;
+                backBlack.IsChecked = false;
+                backBlue.IsChecked = false;
+            }
+        }
+
+        private void textWhite_Click(object sender, RoutedEventArgs e) {
+            textArea.Foreground = Brushes.White;
+            textBlack.IsChecked = false;
+            textRed.IsChecked = false;
+
+            if (textArea.Background == Brushes.White) {
+                textArea.Background = Brushes.Black;
+                backBlack.IsChecked = true;
+                backWhite.IsChecked = false;
+                backBlue.IsChecked = false;
+            }
+        }
+
+        private void textRed_Click(object sender, RoutedEventArgs e) {
+            textArea.Foreground = Brushes.Red;
+            textBlack.IsChecked = false;
+            textWhite.IsChecked = false;
         }
     }
 }
