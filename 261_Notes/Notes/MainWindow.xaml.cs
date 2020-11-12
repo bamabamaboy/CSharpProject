6cb61bb20e3b235ceae304f06625f323c8e5fc09
IXti <ixti@example.com>
2020-11-12 23:02:23 +0300

    Задание 1 к разделу 26 - Блокнот для Заметок

---
+++ 261_Notes/Notes/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Notes {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "*.txt|*.txt";
+            ofd.AddExtension = true;
+
+            bool? result = ofd.ShowDialog();
+
+            if (result != false) {
+                Stream s;
+                if ((s = ofd.OpenFile()) != null) {
+                    string fileName = ofd.FileName;
+                    string fileText = File.ReadAllText(fileName);
+                    textField.Text = fileText;
+                }
+            }
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e) {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "*.txt|*.txt";
+            sfd.AddExtension = true;
+
+            bool? res = sfd.ShowDialog();
+
+            if (res != false) {
+                using (Stream s = File.Open(sfd.FileName, FileMode.OpenOrCreate)) {
+                    using (StreamWriter sw = new StreamWriter(s)) {
+                        sw.Write(textField.Text);
+                    }
+                }
+            }
+        }
+    }
+}
