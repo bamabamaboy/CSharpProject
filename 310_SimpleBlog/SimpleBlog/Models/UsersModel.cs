9fa6d5f8cbab6516b9b7faff74ab2b419cf93c89
IXti <ixti@example.com>
2020-11-20 23:10:12 +0300

    Пример к разделу 32 - Работа с Моделью, Представлением, Контроллером

---
+++ 310_SimpleBlog/SimpleBlog/Models/UsersModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Models {
+    public class UsersModel {
+        public string name;
+        public string surname;
+        public int age;
+        public bool isHasCar = true;
+
+        public UsersModel(string name, string surname, int age, bool isHasCar) {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.isHasCar = isHasCar;
+        }
+    }
+}
