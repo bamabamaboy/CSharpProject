b2a47f38f9a89219d8d6318e28df07ea1b6d3910
IXti <ixti@example.com>
2020-11-21 22:30:31 +0300

    Задание к разделу 32 - Добавил страницу "Статьи"

---
+++ 310_SimpleBlog/SimpleBlog/Controllers/ArticlesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleBlog.Models;
+
+namespace SimpleBlog.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        // GET: Articles
+        public ActionResult Index() {
+            List<ArticlesModel> articles = new List<ArticlesModel>();
+            articles.Add(new ArticlesModel(
+                "Стартап от Google",
+                "Это самый знаменитый стартап, о котором говорит весь мир",
+                "Мистер Пупкин"));
+            articles.Add(new ArticlesModel(
+                "Немного о Facebook",
+                "Facebook способствовал увеличению голосов Байдена на выборах",
+                "Мистер Пупкинский"));
+            articles.Add(new ArticlesModel(
+                "Новости из Кремниевой Долины",
+                "ИТ-гиганты со всего мира стремяться попасть скорее туда",
+                "Мистер ИТшник"));
+
+            return View(articles);
+        }
+    }
+}
---
+++ 310_SimpleBlog/SimpleBlog/Models/ArticlesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Models {
+    public class ArticlesModel {
+
+        public string header;
+        public string text;
+        public string author;
+
+        public ArticlesModel(string header, string text, string author) {
+            this.header = header;
+            this.text = text;
+            this.author = author;
+        }
+    }
+}
