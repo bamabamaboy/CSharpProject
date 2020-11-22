81c03f0ca7b5aa2dfae06da3b20228ca1f5cb1e0
IXti <ixti@example.com>
2020-11-22 22:32:24 +0300

    Пример к разделу 33 - Форма авторизации на сайте

---
+++ 330_AuthProject/AuthProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthProject.Startup))]
+namespace AuthProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
